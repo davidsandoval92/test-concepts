bd503bb2467158f72e94594a93cbf63096697623
David Sandoval <davidsandoval9217@gmail.com>
2019-04-14 16:51:32 -0500

    Refactor class date solution.

---
+++ src/app/date-solution/constants-year.ts
@@ -0,0 +1,14 @@
+export const CONSTANTS_YEAR: any[] =
+    new Array(
+        { id: 0, name: 'ENERO' },
+        { id: 1, name: 'FEBRERO' },
+        { id: 2, name: 'MARZO' },
+        { id: 3, name: 'ABRIL' },
+        { id: 4, name: 'MAYO' },
+        { id: 5, name: 'JUNIO' },
+        { id: 6, name: 'JULIO' },
+        { id: 7, name: 'AGOSTO' },
+        { id: 8, name: 'SEPTIEMBRE' },
+        { id: 9, name: 'OCTUBRE' },
+        { id: 10, name: 'NOVIEMBRE' },
+        { id: 11, name: 'DICIEMBRE' });
--- src/app/date-solution/date-solution.component.ts
+++ src/app/date-solution/date-solution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CONSTANTS_YEAR } from './constants-year';
 
 @Component({
   selector: 'app-date-solution',
@@ -11,22 +12,7 @@ export class DateSolutionComponent implements OnInit {
   public dateObtained: string;
   public year: string;
   public month: string;
-  public months = [
-    { id: 0, name: 'ENERO' },
-    { id: 1, name: 'FEBRERO' },
-    { id: 2, name: 'MARZO' },
-    { id: 3, name: 'ABRIL' },
-    { id: 4, name: 'MAYO' },
-    { id: 5, name: 'JUNIO' },
-    { id: 6, name: 'JULIO' },
-    { id: 7, name: 'AGOSTO' },
-    { id: 8, name: 'SEPTIEMBRE' },
-    { id: 9, name: 'OCTUBRE' },
-    { id: 10, name: 'NOVIEMBRE' },
-    { id: 11, name: 'DICIEMBRE' },
-  ];
   public monthsSelected;
-  public readonly MONTHS = 12;
 
   constructor() {
     this.monthsSelected = new Array();
@@ -41,14 +27,12 @@ export class DateSolutionComponent implements OnInit {
   public getDate(): void {
     this.dateCurrently = new Date();
     const newDate = new Date(this.dateObtained);
-    this.months.forEach((item, i) => {
+    CONSTANTS_YEAR.forEach((item, i) => {
       if (i >= newDate.getMonth()) {
-        console.log(item.name);
         this.monthsSelected.push(item);
       }
     }
     );
-    console.log(newDate.getFullYear());
   }
 
   public obtainYearAndMonth(): void {
